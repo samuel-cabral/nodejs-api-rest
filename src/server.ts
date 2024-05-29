e9aef658bc77b580640971b6ed9c8ea3ca336326
Samuel Cabral <samuelcabral.mail@gmail.com>
2024-05-29 15:43:34 -0300

    feat: create app

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get("/", () => {
+  return { hello: "world" };
+});
+
+app.listen({
+  port: 3333,
+}).then(() => {
+  console.log("Server is running on http://localhost:3333");
+});
