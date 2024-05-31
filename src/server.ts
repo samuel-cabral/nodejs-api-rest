9cfd5a8034c2d282f6365b80bfb3542141fbcb31
Samuel Cabral <samuelcabral.mail@gmail.com>
2024-05-31 18:34:05 -0300

    chore: Add preHandler global hook for logging requests

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,11 @@ import { transactionsRoutes } from './routes/transactions'
 const app = fastify()
 
 app.register(cookie)
+
+app.addHook('preHandler', async (request) => {
+  console.log(`[${request.method}] ${request.url}`)
+})
+
 app.register(transactionsRoutes, {
   prefix: '/transactions',
 })
