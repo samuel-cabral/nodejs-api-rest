cf7ac4d21202a53ef52a8eb83ea6a0dec4ff3ed7
Samuel Cabral <samuelcabral.mail@gmail.com>
2024-10-08 19:44:38 -0300

    test: it should be able to creata a new transaction

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,4 @@
-import fastify from 'fastify'
-import cookie from '@fastify/cookie'
-import { transactionsRoutes } from './routes/transactions'
-
-const app = fastify()
-
-app.register(cookie)
-
-app.addHook('preHandler', async (request) => {
-  console.log(`[${request.method}] ${request.url}`)
-})
-
-app.register(transactionsRoutes, {
-  prefix: '/transactions',
-})
+import { app } from './app'
 
 app
   .listen({
