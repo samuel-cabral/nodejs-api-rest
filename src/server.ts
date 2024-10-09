1c7ab95b75f9f3d7b639d517fb1730bb48ebc526
Samuel Cabral <samuelcabral.mail@gmail.com>
2024-10-09 00:45:02 -0300

    fix: PORT

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
-import { env } from 'process'
+import { env } from './env'
 import { app } from './app'
 
 app
   .listen({
     host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
+    port: env.PORT,
   })
   .then(() => {
     console.log(`Server is running on http://localhost:${env.PORT}`)
